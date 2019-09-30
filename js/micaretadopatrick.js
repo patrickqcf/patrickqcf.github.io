9d2b385c9390eb2c980dfcf82cf678becea65500
patrickqcf <45515053+patrickqcf@users.noreply.github.com>
2019-09-30 17:06:16 -0300

    Adicionado primeira versão do relógio

---
+++ js/micaretadopatrick.js
@@ -0,0 +1,42 @@
+var countDownDate = new Date("Oct 26, 2019 00:00:00").getTime();
+
+var contador = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  if(seconds < 10){
+      seconds ="0" + seconds;
+  }
+  if(minutes < 10){
+      minutes ="0" + minutes;
+  }
+  if(hours < 10){
+      hours ="0" + hours;
+  }
+  if(days < 10){
+      days ="0" + days;
+  }
+
+  $('.dia1').html(days.toString().substring(0,1));
+  $('.dia2').html(days.toString().substring(1,2));
+  $('.hora1').html(hours.toString().substring(0,1));
+  $('.hora2').html(hours.toString().substring(1,2));
+  $('.minuto1').html(minutes.toString().substring(0,1));
+  $('.minuto2').html(minutes.toString().substring(1,2));
+  $('.segundo1').html(seconds.toString().substring(0,1));
+  $('.segundo2').html(seconds.toString().substring(1,2));
+
+  $('#countdownTimer').html(days + "d " + hours + ":"  + minutes + ":" + seconds);
+
+
+  if (distance < 0) {
+    clearInterval(contador);
+    $('#countdownTimer').html("ACREDITE NOS SEUS SONHOS");
+  }
+}, 1000);
